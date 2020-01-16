acee27bcbc4f143342957bc41ab3da9e6f1981f7
TirthAgrawal12 <43172416+TirthAgrawal12@users.noreply.github.com>
2020-01-16 10:19:41 +0530

    user preference added
    User can now see desired URL.

--- 17it003_WCMC_PR9/app/src/main/java/com/example/a17it003_wcmc_pr9/MainActivity.java
+++ 17it003_WCMC_PR9/app/src/main/java/com/example/a17it003_wcmc_pr9/MainActivity.java
@@ -2,18 +2,37 @@ package com.example.a17it003_wcmc_pr9;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     WebView webView;
+    EditText URL;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        webView = (WebView) findViewById(R.id.webView);
-        webView.loadUrl("https://www.linkedin.com/in/tirth-agrawal-371377168/");
+        btn = (Button)findViewById(R.id.btn1);
+        URL = (EditText)findViewById(R.id.et1);
+
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                String s = URL.getText().toString();
+                webView = (WebView) findViewById(R.id.webView);
+                webView.loadUrl(s);
+
+            }
+        });
 
 
     }
